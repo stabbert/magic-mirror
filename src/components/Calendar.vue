b34278a7b16c2b0d590fd5841a96d677c2f1700b
stabbert <MeS1981@gmx.de>
2020-12-26 15:02:05 +0100

    Change from moment to time in milliseconds. Furthermore, exceptions in repeating events are considered.

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -25,11 +25,11 @@ import ICAL from "ical.js";
 import moment from "moment";
 
 // Define second, minute, hour, and day variables
-const oneSecond = 1000; // 1,000 milliseconds
-const oneMinute = oneSecond * 60;
-const oneHour = oneMinute * 60;
-const oneDay = oneHour * 24;
-const twoDay = oneDay * 2;
+const oneSecondInMs = 1000; // 1,000 milliseconds
+const oneMinuteInMs = oneSecondInMs * 60;
+const oneHourInMs = oneMinuteInMs * 60;
+const oneDayInMs = oneHourInMs * 24;
+const twoDayInMs = oneDayInMs * 2;
 
 function shorten(string, maxLength, wrapEvents) {
   if (typeof string !== "string") {
@@ -81,23 +81,23 @@ function isFullDayEvent(event) {
   return event.duration.days > 0 ? true : false;
 }
 
-function calculateTimeTitle(now, newEvent) {
+function calculateTimeTitle(nowTimeInMs, newEvent) {
   if (newEvent.fullDayEvent) {
-    const startOnlyDate = newEvent.startDate.clone().startOf("date");
-    const endOnlyDate = newEvent.endDate.clone().startOf("date");
+    const startTimeInMsOnlyDate = startOfDayAsTimeInMs(newEvent.startTimeInMs);
+    const endTimeInMsOnlyDate = startOfDayAsTimeInMs(newEvent.endTimeInMs);
     if (
-      newEvent.startDate.isSame(startOnlyDate) &&
-      newEvent.endDate.isSame(endOnlyDate)
+      isSame(newEvent.startTimeInMs, startTimeInMsOnlyDate) &&
+      isSame(newEvent.endTimeInMs, endTimeInMsOnlyDate)
     ) {
-      const nowOnlyDate = now.clone().startOf("date");
-      if (startOnlyDate.isSame(nowOnlyDate)) {
+      const nowTimeInMsOnlyDate = startOfDayAsTimeInMs(nowTimeInMs);
+      if (isSame(startTimeInMsOnlyDate, nowTimeInMsOnlyDate)) {
         return "Heute";
       } else {
-        const diff = newEvent.startDate.diff(now);
-        if (diff > 0) {
-          if (diff < oneDay) {
+        const diffInMs = newEvent.startTimeInMs - nowTimeInMs;
+        if (diffInMs > 0) {
+          if (diffInMs < oneDayInMs) {
             return "Morgen";
-          } else if (diff < twoDay) {
+          } else if (diffInMs < twoDayInMs) {
             return "Übermorgen";
           }
         }
@@ -105,18 +105,21 @@ function calculateTimeTitle(now, newEvent) {
     }
   }
 
-  if (newEvent.startDate.isAfter(now)) {
-    if (newEvent.startDate.diff(now) < twoDay) {
-      return capFirst(newEvent.startDate.calendar());
+  if (isAfter(newEvent.startTimeInMs, nowTimeInMs)) {
+    const diffInMs = newEvent.startTimeInMs - nowTimeInMs;
+    if (diffInMs < twoDayInMs) {
+      return capFirst(moment(newEvent.startTimeInMs).calendar());
     } else {
-      const startOnlyDate = newEvent.startDate.clone().startOf("date");
-      const nowOnlyDate = now.clone().startOf("date");
-      return capFirst(startOnlyDate.from(nowOnlyDate));
+      const startTimeInMsOnlyDate = startOfDayAsTimeInMs(
+        newEvent.startTimeInMs
+      );
+      const nowTimeInMsOnlyDate = startOfDayAsTimeInMs(nowTimeInMs);
+      return capFirst(moment(startTimeInMsOnlyDate).from(nowTimeInMsOnlyDate));
     }
-  } else if (newEvent.endDate.isAfter(now)) {
-    return "Noch " + newEvent.endDate.from(now, true);
+  } else if (isAfter(newEvent.endTimeInMs, nowTimeInMs)) {
+    return "Noch " + moment(newEvent.endTimeInMs).from(nowTimeInMs, true);
   } else {
-    return capFirst(newEvent.endDate.from(now));
+    return capFirst(moment(newEvent.endTimeInMs).from(nowTimeInMs));
   }
 }
 
@@ -134,6 +137,43 @@ function uniq(array) {
   }
 
   return result;
+}
+
+function isBefore(timeInMsOne, timeInMsTwo) {
+  return timeInMsOne < timeInMsTwo;
+}
+
+function isAfter(timeInMsOne, timeInMsTwo) {
+  return timeInMsOne > timeInMsTwo;
+}
+
+function isSame(timeInMsOne, timeInMsTwo) {
+  return timeInMsOne === timeInMsTwo;
+}
+
+function isBetween(timeInMs, fromTimeInMs, toTimeInMs) {
+  return timeInMs >= fromTimeInMs && timeInMs <= toTimeInMs;
+}
+
+function startOfDayAsTimeInMs(timeInMs) {
+  const date = new Date(timeInMs);
+  date.setHours(0, 0, 0, 0);
+  return date.getTime();
+}
+
+function addDaysAsTimeInMs(timeInMs, days) {
+  const date = new Date(timeInMs);
+  date.setDate(date.getDate() + days);
+  return date.getTime();
+}
+
+function toTimeInMs(icaltime) {
+  return icaltime.toJSDate().getTime();
+}
+
+function fromRuleNextTimeInMs(rule) {
+  const next = rule.next();
+  return next && toTimeInMs(next);
 }
 
 export default {
@@ -160,12 +200,10 @@ export default {
         clearInterval(updateTimeInAllEventsIntervalId);
       }
 
-      const now = moment();
-      const today = now.clone().startOf("date");
-      const future = today
-        .clone()
-        .add(config.maximumNumberOfDays, "days")
-        .subtract(1, "seconds"); // Subtract 1 second so that events that start on the middle of the night will not repeat.
+      const nowTimeInMs = Date.now();
+      const todayTimeInMs = startOfDayAsTimeInMs(nowTimeInMs);
+      const futureTimeInMs =
+        addDaysAsTimeInMs(nowTimeInMs, config.maximumNumberOfDays) - 1000; // Subtract 1 second so that events that start on the middle of the night will not repeat.
 
       let calendarFetches = [];
 
@@ -211,13 +249,13 @@ export default {
                   continue;
                 }
 
-                let startDate = moment(event.startDate.toJSDate());
-                let endDate;
+                let startTimeInMs = toTimeInMs(event.startDate);
+                let endTimeInMs;
 
                 if (typeof event.endDate !== "undefined") {
-                  endDate = moment(event.endDate.toJSDate());
+                  endTimeInMs = toTimeInMs(event.endDate);
                 } else {
-                  endDate = startDate;
+                  endTimeInMs = startTimeInMs;
                 }
 
                 let title = "Termin";
@@ -232,68 +270,73 @@ export default {
 
                 // Replaying event
                 if (event.isRecurring()) {
-                  let dates = [];
+                  let timesInMs = [];
                   let rule = event.iterator();
+                  let exceptionTimesInMs = rule.exDates.map(exceptionDate =>
+                    toTimeInMs(exceptionDate)
+                  );
                   let shiftedRecurrenceEvent =
                     shiftedRecurrenceEventMap[event.uid];
 
-                  let shiftedMoment = shiftedRecurrenceEvent
-                    ? moment(shiftedRecurrenceEvent.recurrenceId.toJSDate())
+                  let shiftedTimeInMs = shiftedRecurrenceEvent
+                    ? toTimeInMs(shiftedRecurrenceEvent.recurrenceId)
                     : null;
 
-                  let nextDate = rule.next();
-                  while (nextDate) {
-                    let nextMoment = moment(nextDate.toJSDate());
-                    if (shiftedMoment && shiftedMoment.isSame(nextMoment)) {
-                      nextDate = rule.next();
+                  let nextTimeInMs = fromRuleNextTimeInMs(rule);
+                  while (nextTimeInMs) {
+                    if (
+                      shiftedTimeInMs &&
+                      isSame(nextTimeInMs, shiftedTimeInMs)
+                    ) {
+                      nextTimeInMs = fromRuleNextTimeInMs(rule);
                       continue;
                     }
                     if (
-                      nextMoment.isBetween(today, future) ||
-                      nextMoment.isSame(today)
+                      isBetween(nextTimeInMs, todayTimeInMs, futureTimeInMs) &&
+                      exceptionTimesInMs.indexOf(nextTimeInMs) === -1
                     ) {
-                      dates.push(nextMoment);
+                      timesInMs.push(nextTimeInMs);
                     }
                     if (
-                      dates.length <= config.maximumEntries &&
-                      nextMoment.isBefore(future)
+                      timesInMs.length <= config.maximumEntries &&
+                      isBefore(nextTimeInMs, futureTimeInMs)
                     ) {
-                      nextDate = rule.next();
+                      nextTimeInMs = fromRuleNextTimeInMs(rule);
                     } else {
-                      nextDate = false;
+                      nextTimeInMs = false;
                     }
                   }
 
                   // calculate the duration of the event for use with recurring events.
-                  let duration = endDate.diff(startDate);
+                  let durationTimeInMs = endTimeInMs - startTimeInMs;
 
-                  for (let d in dates) {
-                    startDate = dates[d];
-                    endDate = startDate.clone().add(duration);
+                  for (let timeInMs of timesInMs) {
+                    startTimeInMs = timeInMs;
+                    endTimeInMs = startTimeInMs + durationTimeInMs;
 
-                    if (endDate.isAfter(now)) {
+                    if (isAfter(endTimeInMs, nowTimeInMs)) {
                       newEvents.push({
                         title: title,
-                        startDate: startDate,
-                        endDate: endDate,
+                        startTimeInMs: startTimeInMs,
+                        endTimeInMs: endTimeInMs,
                         fullDayEvent: isFullDayEvent(event)
                       });
                     }
                   }
                 } else {
-                  if (endDate.isSameOrBefore(now)) {
+                  if (isBefore(endTimeInMs, nowTimeInMs)) {
                     continue;
                   }
 
-                  if (startDate.isAfter(future)) {
+                  if (isAfter(startTimeInMs, futureTimeInMs)) {
                     continue;
                   }
 
                   // Every thing is good. Add it to the list.
                   newEvents.push({
                     title: title,
-                    startDate: startDate,
-                    endDate: endDate,
+                    startTimeInMs: startTimeInMs,
+                    endTimeInMs: endTimeInMs,
                     fullDayEvent: isFullDayEvent(event)
                   });
                 }
@@ -315,7 +358,7 @@ export default {
         );
 
         allNewEvents.sort(function(a, b) {
-          return a.startDate.valueOf() - b.startDate.valueOf();
+          return a.startTimeInMs - b.startTimeInMs;
         });
 
         let maximumAllNewEvents = allNewEvents.slice(0, config.maximumEntries);
@@ -341,7 +384,7 @@ export default {
               config.maxTitleLength,
               config.wrapEvents
             ),
-            time: calculateTimeTitle(now, newEvent),
+            time: calculateTimeTitle(nowTimeInMs, newEvent),
             symbol:
               newEvent.title.indexOf("Geburtstag") === -1
                 ? "fa-calendar-check-o"
@@ -351,15 +394,15 @@ export default {
         });
 
         function updateTimeInAllEvents() {
-          const now = moment();
+          const nowTimeInMs = Date.now();
           maximumAllNewEvents.forEach((newEvent, index) => {
-            self.events[index].time = calculateTimeTitle(now, newEvent);
+            self.events[index].time = calculateTimeTitle(nowTimeInMs, newEvent);
           });
         }
 
         updateTimeInAllEventsIntervalId = setInterval(
           updateTimeInAllEvents,
-          oneMinute
+          oneMinuteInMs
         );
       });
     }
