7ce34d4eda1d8853738161de0c41e123205dbf39
stabbert <MeS1981@gmx.de>
2022-02-21 15:43:11 +0100

    [Bugfix] Set minimum target build version

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "chrome65",
+  },
   plugins: [vue()],
   preview: {
     port: 8080,
